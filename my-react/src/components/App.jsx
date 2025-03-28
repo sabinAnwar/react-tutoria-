8e0a34255fa5ba74ac8a4ff3c6f7deac55295f1a
Sabin <sabinanwar6@gmail.com>
2025-03-28 15:55:41 +0100

    add Babel and Webpack configurations, update dependencies, and create App component

---
+++ my-react/src/components/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+export class App extends React.Component {
+  render() {
+    return <div>Sabin El Anwar</div>;
+  }
+}
--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App.jsx";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
-reportWebVitals();
