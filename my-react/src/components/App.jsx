7692fb26c635a24d6063ed08aa08ef454558fc3a
Sabin <sabinanwar6@gmail.com>
2025-03-28 17:53:24 +0100

    implement countdown timer with hooks and cleanup on unmount

--- my-react/src/components/App.jsx
+++ my-react/src/components/App.jsx
@@ -1,51 +1,75 @@
-import React, { useState } from "react";
-export default function App(){
-//   state = {
-//     counterValue: 10,
-//   };
-//   componentDidMount() {
-//     this.myInterval = setInterval(() => {
-//       this.setState((state) => {
-//         if (state.counterValue > 0) {
-//           return { counterValue: state.counterValue - 1 };
-//         } else {
-//           return { counterValue: 0 };
-//         }
-//       });
-//     }, 1000); // Decrease the counter value every second
-//   }
-//   componentWillUnmount() {
-//     clearInterval(this.myInterval);
-//   }
-//   // componentDidUpdate() {
-//   componentDidUpdate(prevProps, prevState) {
-//     console.log(prevProps, prevState);
-//   }
-// shouldComponentUpdate(){
-//   if(this.state.counterValue === 0) {
-//     return false; // Prevent re-rendering when counterValue is 0
-//   }
-//   return true; // Allow re-rendering for other cases
-// }
-//   return <strong>
-//     {this.props.children} 
-//     <br />
-//     {/* <h1>Counter Value: {this.state.counterValue}</h1> */}
-//     {/* <h1>Counter Value: {this.props.children}</h1> */}
-//     <h1>Counter Value: {this.props.children}</h1>
-//     {/* <h1>Counter Value: {this.state.counterValue}</h1> */}
-//   </strong>;
-// }
+import React, { useState, useEffect } from "react";
+// export default function App(){
+// //   state = {
+// //     counterValue: 10,
+// //   };
+// //   componentDidMount() {
+// //     this.myInterval = setInterval(() => {
+// //       this.setState((state) => {
+// //         if (state.counterValue > 0) {
+// //           return { counterValue: state.counterValue - 1 };
+// //         } else {
+// //           return { counterValue: 0 };
+// //         }
+// //       });
+// //     }, 1000); // Decrease the counter value every second
+// //   }
+// //   componentWillUnmount() {
+// //     clearInterval(this.myInterval);
+// //   }
+// //   // componentDidUpdate() {
+// //   componentDidUpdate(prevProps, prevState) {
+// //     console.log(prevProps, prevState);
+// //   }
+// // shouldComponentUpdate(){
+// //   if(this.state.counterValue === 0) {
+// //     return false; // Prevent re-rendering when counterValue is 0
+// //   }
+// //   return true; // Allow re-rendering for other cases
+// // }
+// //   return <strong>
+// //     {this.props.children} 
+// //     <br />
+// //     {/* <h1>Counter Value: {this.state.counterValue}</h1> */}
+// //     {/* <h1>Counter Value: {this.props.children}</h1> */}
+// //     <h1>Counter Value: {this.props.children}</h1>
+// //     {/* <h1>Counter Value: {this.state.counterValue}</h1> */}
+// //   </strong>;
+// // }
 
-    const [counterState, setCounterState] = useState(0);
+//     const [counterState, setCounterState] = useState(0);
 
-    return (
-      <div>
-        <button onClick={() => {
-          setCounterState(counterState + 1);
-        }}>Click me</button>
-        <p>Counter: {counterState}</p>
-      </div>
-    );
+//     return (
+//       <div>
+//         <button onClick={() => {
+//           setCounterState(counterState + 1);
+//         }}>Click me</button>
+//         <p>Counter: {counterState}</p>
+//       </div>
+//     );
 
+// }import React, { useState, useEffect } from 'react';
+
+export default function App() {
+  const [countdown, setCountdown] = useState(10);
+
+  useEffect(() => {
+    // componentDidMount
+    const ref = setInterval(() => {
+      if (countdown > 0) {
+        setCountdown(countdown - 1);
+      }
+    }, 1000);
+
+    // Optional: cleanup on unmount
+    return () => clearInterval(ref);
+  }, [countdown]);
+
+  return (
+    <div>
+      <h1>Countdown</h1>
+      <p>{countdown}</p>
+      <progress value={countdown} min="0" max="10" />
+    </div>
+  );
 }
