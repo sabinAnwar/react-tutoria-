b33a46d5e154d884925b1427e021777522e36d6b
Sabin <sabinanwar6@gmail.com>
2025-04-01 00:05:00 +0200

    User-Interaktionen abfangen

--- my-react/src/components/Course.jsx
+++ my-react/src/components/Course.jsx
@@ -1,20 +1,44 @@
-import propTypes from "prop-types";
-function Course(props) {
+// import propTypes from "prop-types";
+// function Course(props) {
+//   return (
+//     <div>
+//       <p>{props.name}</p>
+//       <p>{props.link1}</p>
+//       <p>{props.link2}</p>
+//       <p>{props.link3}</p>
+//     </div>
+//   );
+// }
+
+// Course.propTypes = {
+//   name: propTypes.string,
+//   link1: propTypes.string, // <-- Das hier prüft den Typ
+//   link2: propTypes.string,
+//   link3: propTypes.string,
+// };
+
+// export default Course;
+import React, { useState } from "react";
+
+export default function App() {
+  const [headline, setHeadline] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const buttonClicked = () => {
+    setHeadline(inputValue);
+  };
+
+  const inputChanged = (event) => {
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  };
+
   return (
     <div>
-      <p>{props.name}</p>
-      <p>{props.link1}</p>
-      <p>{props.link2}</p>
-      <p>{props.link3}</p>
+      <h1>{headline}</h1>
+      <input type="text" onInput={inputChanged} />
+      <br />
+      <button onClick={buttonClicked}>Make me big!</button>
     </div>
   );
 }
-
-Course.propTypes = {
-  name: propTypes.string,
-  link1: propTypes.string, // <-- Das hier prüft den Typ
-  link2: propTypes.string,
-  link3: propTypes.string,
-};
-
-export default Course;
--- my-react/src/index.js
+++ my-react/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App2 from "./components/App2"; // Ensure the App component exists in the specified path
+import Course from "./components/Course"; // Ensure the App component exists in the specified path
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App2 />
+    <Course />
   </React.StrictMode>
 );
